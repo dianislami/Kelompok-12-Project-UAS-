130c5b3c8c32b6eaf402d15aeba3bed10393633c
dian islami <163720568+dianislami@users.noreply.github.com>
2024-05-14 15:05:25 +0700

    Update header.h

--- header.h
+++ header.h
@@ -17,7 +17,7 @@ int Login();
 void bacaDataBarang(Produk dataproduk[], int *jumlahproduk);
 void tulisDataBarang(Produk dataproduk[], int jumlahproduk);
 void tambahBarang(Produk dataproduk[], int *jumlahproduk);
-void kurangBarang(Produk dataproduk[], int *jumlahproduk);
+void hapusBarang(Produk dataproduk[], int *jumlahproduk);
 void tampilkanDaftarBarang(Produk dataproduk[], int jumlahproduk);
 void pembayaran(Produk dataproduk[], int jumlahproduk);
 void tambahStok(Produk dataproduk[], int jumlahproduk);
