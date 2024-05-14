5f898d83d6ab766580f979689b1617c80af5182c
Muhammad Sidqi Alfareza <163630718+SQmojitocola@users.noreply.github.com>
2024-05-15 00:59:57 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include "header.h"
 
+//menampilkan menu utama dari program secara keseluruhan
 int main() {
     Produk dataproduk[MAX_BARANG];
     int jumlahproduk = 0;
     int pilihan;
     int nilai;
 
+    //membaca data yang ada di dalam file external data barang
     bacaDataBarang(dataproduk, &jumlahproduk);
 
     //Login sebelum masuk ke menu kasir
@@ -27,30 +29,37 @@ int main() {
 
         //Kode program berupa fungsi untuk setiap menu
         switch (pilihan) {
+            //menu untuk menambah barang
             case 1:
                 tambahBarang(dataproduk, &jumlahproduk);
                 tulisDataBarang(dataproduk, jumlahproduk);
                 break;
+            //menu untuk menghapus barang 
             case 2:
                 hapusBarang(dataproduk, &jumlahproduk);
                 tulisDataBarang(dataproduk, jumlahproduk);
                 break;
+            //menu untuk menambah stok dari barang yang sudah ada di dalam ada di file external
             case 3:
                 tambahStok(dataproduk, jumlahproduk);
                 tulisDataBarang(dataproduk, jumlahproduk);
                 break;
+            //menu untuk mengurangi stok dari barang yang sudah ada di dalam ada di file external
             case 4:
                 kurangiStok(dataproduk, jumlahproduk);
                 tulisDataBarang(dataproduk, jumlahproduk);
                 break;
+            //menu untuk menampilkan barang barang yang telah tertera di file external
             case 5:
                 tampilkanDaftarBarang(dataproduk, jumlahproduk);
                 tulisDataBarang(dataproduk, jumlahproduk);
                 break;
+            //menu untuk memproses pembayaran yang ada di dalam program
             case 6:
                 pembayaran(dataproduk, jumlahproduk);
                 tulisDataBarang(dataproduk, jumlahproduk);
                 break;
+            //menu untuk keluar dari program
             case 7:
                 exit(0);
             default:
